802e94cf35c40d86bffa7ddff14f1494cf86516f
claudijo <claudijo.borovic@gmail.com>
2015-05-18 09:39:07 +0000

    Change variable names in unit test, to reflect project name.

--- test/index.js
+++ test/index.js
@@ -1,40 +1,40 @@
 var assert = require('assert');
-var Smorgasboard = require('..');
+var DiModular = require('..');
 
-describe('Smorgasboard', function() {
-  var sb;
+describe('DiModular', function() {
+  var modular;
 
   beforeEach(function() {
-    sb = new Smorgasboard();
+    modular = new DiModular();
   });
 
   it('should be possible to instantiate without using the new keyboard', function() {
-    var newSb = Smorgasboard();
-    assert(newSb instanceof Smorgasboard);
+    var newSb = DiModular();
+    assert(newSb instanceof DiModular);
   });
 
   it('should return registered dependency when calling get', function() {
-    sb.register('someDependency', 'someValue');
-    assert(sb.get('someDependency') === 'someValue');
+    modular.register('someDependency', 'someValue');
+    assert(modular.get('someDependency') === 'someValue');
   });
 
   it('should return instance from registered factory when calling get', function() {
-    sb.factory('someFactory', function() {
+    modular.factory('someFactory', function() {
       return {
         getSomeValue: function() { return 'someValue'; }
       }
     });
-    assert(sb.get('someFactory').getSomeValue() === 'someValue');
+    assert(modular.get('someFactory').getSomeValue() === 'someValue');
   });
 
   it('should call init on module when starting registered module', function() {
     var callCount = 0;
-    sb.module('someModule', function() {
+    modular.module('someModule', function() {
       return {
         init: function() { callCount++; }
       }
     });
-    sb.start('someModule');
+    modular.start('someModule');
     assert(callCount === 1);
   });
 
@@ -45,16 +45,16 @@ describe('Smorgasboard', function() {
         that = this;
       }
     };
-    sb.module('someModule', function() {
+    modular.module('someModule', function() {
       return someModule;
     });
-    sb.start('someModule');
+    modular.start('someModule');
     assert(that === someModule);
   });
 
   it('should call init with arguments provided when starting registered module', function() {
     var firstArgument, secondArgument;
-    sb.module('someModule', function() {
+    modular.module('someModule', function() {
       return {
         init: function() {
           firstArgument = arguments[0];
@@ -62,59 +62,59 @@ describe('Smorgasboard', function() {
         }
       }
     });
-    sb.start('someModule', 'firstArgument', 'secondArgument');
+    modular.start('someModule', 'firstArgument', 'secondArgument');
     assert(firstArgument === 'firstArgument');
     assert(secondArgument === 'secondArgument');
   });
 
   it('should not call init when starting already started module', function() {
     var callCount = 0;
-    sb.module('someModule', function() {
+    modular.module('someModule', function() {
       return {
         init: function() {
           callCount++;
         }
       }
     });
-    sb.start('someModule');
+    modular.start('someModule');
     assert(callCount === 1);
-    sb.start('someModule');
+    modular.start('someModule');
     assert(callCount === 1);
   });
 
   it('should call init on all modules when starting all registered modules', function() {
     var callCount = 0;
-    sb.module('firstModule', function() {
+    modular.module('firstModule', function() {
       return {
         init: function() { callCount++; }
       };
     });
-    sb.module('secondModule', function() {
+    modular.module('secondModule', function() {
       return {
         init: function() { callCount++; }
       };
     });
-    sb.startAll();
+    modular.startAll();
     assert(callCount === 2);
   });
 
   it('should call init with arguments provided when starting all registered modules', function() {
     var firstModuleArguments, secondModuleArguments;
-    sb.module('firstModule', function() {
+    modular.module('firstModule', function() {
       return {
         init: function() {
           firstModuleArguments = Array.prototype.slice.call(arguments);
         }
       };
     });
-    sb.module('b', function() {
+    modular.module('b', function() {
       return {
         init: function() {
           secondModuleArguments = Array.prototype.slice.call(arguments);
         }
       };
     });
-    sb.startAll('firstArgument', 'secondArgument');
+    modular.startAll('firstArgument', 'secondArgument');
 
     assert(firstModuleArguments[0] === 'firstArgument');
     assert(firstModuleArguments[1] === 'secondArgument');
@@ -124,13 +124,13 @@ describe('Smorgasboard', function() {
 
   it('should call destroy on module when stopping registered module', function() {
     var callCount = 0;
-    sb.module('someModule', function() {
+    modular.module('someModule', function() {
       return {
         destroy: function() { callCount++;}
       }
     });
-    sb.start('someModule');
-    sb.stop('someModule');
+    modular.start('someModule');
+    modular.stop('someModule');
     assert(callCount === 1);
   });
 
@@ -141,85 +141,85 @@ describe('Smorgasboard', function() {
         that = this;
       }
     };
-    sb.module('someModule', function() {
+    modular.module('someModule', function() {
       return module;
     });
-    sb.start('someModule');
-    sb.stop('someModule');
+    modular.start('someModule');
+    modular.stop('someModule');
     assert(that === module);
   });
 
   it('should not call destroy on module when module has not been started yet', function() {
     var callCount = 0;
-    sb.module('someModule', function() {
+    modular.module('someModule', function() {
       return {
         destroy: function() { callCount++;}
       }
     });
-    sb.stop('someModule');
+    modular.stop('someModule');
     assert(callCount === 0);
   });
 
   it('should not call destroy on module when module has already been stopped', function() {
     var callCount = 0;
-    sb.module('someModule', function() {
+    modular.module('someModule', function() {
       return {
         destroy: function() { callCount++;}
       }
     });
-    sb.start('someModule');
-    sb.stop('someModule');
+    modular.start('someModule');
+    modular.stop('someModule');
     assert(callCount === 1);
-    sb.stop('someModule');
+    modular.stop('someModule');
     assert(callCount === 1);
   });
 
   it('should call destroy on all modules when stopping all registered and started modules', function() {
     var callCount = 0;
-    sb.module('firstModule', function() {
+    modular.module('firstModule', function() {
       return {
         destroy: function() { callCount++; }
       };
     });
-    sb.module('secondModule', function() {
+    modular.module('secondModule', function() {
       return {
         destroy: function() { callCount++; }
       };
     });
-    sb.startAll();
-    sb.stopAll();
+    modular.startAll();
+    modular.stopAll();
     assert(callCount === 2);
   });
 
   it('should inject registered dependency', function() {
     var someDependency;
-    sb.register('$someDependency', 'someValue');
-    sb.module('someModule', function($someDependency) {
+    modular.register('$someDependency', 'someValue');
+    modular.module('someModule', function($someDependency) {
       return {
         init: function() {
           someDependency = $someDependency;
         }
       }
     });
-    sb.start('someModule');
+    modular.start('someModule');
     assert(someDependency === 'someValue');
   });
 
   it('should inject instances from registered factory', function() {
     var someValue;
-    sb.factory('$someFactory', function() {
+    modular.factory('$someFactory', function() {
       return {
         someValue: 'someValue'
       };
     });
-    sb.module('someModule', function($someFactory) {
+    modular.module('someModule', function($someFactory) {
       return {
         init: function() {
           someValue = $someFactory.someValue;
         }
       }
     });
-    sb.start('someModule');
+    modular.start('someModule');
     assert(someValue === 'someValue');
   });
 });
